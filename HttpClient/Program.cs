91b4663e4abcde4bf1d931ee9609b4053f1c53dd
bubibubi <u.ballestrazzi@gmail.com>
2017-02-27 19:05:55 +0100

    Add project files.

---
+++ HttpClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace HttpClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync("http://www.google.com").Result;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string text = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine("{0} - {1}", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
